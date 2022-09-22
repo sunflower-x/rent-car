443fe462f7997c28459f73f997f29746a249fd8d
zmj <1639218509@qq.com>
2022-09-22 09:17:44 +0800

    删除无用

--- src/main/java/com/ikun/controller/InspectBillController.java
+++ src/main/java/com/ikun/controller/InspectBillController.java
@@ -18,67 +18,6 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("inspectBill")
 public class InspectBillController {
-    /**
-     * 服务对象
-     */
-    @Resource
-    private InspectBillService inspectBillService;
-
-    /**
-     * 分页查询
-     *
-     * @param inspectBill 筛选条件
-     * @param pageRequest      分页对象
-     * @return 查询结果
-     */
-    @GetMapping
-    public ResponseEntity<Page<InspectBill>> queryByPage(InspectBill inspectBill, PageRequest pageRequest) {
-        return ResponseEntity.ok(this.inspectBillService.queryByPage(inspectBill, pageRequest));
-    }
-
-    /**
-     * 通过主键查询单条数据
-     *
-     * @param id 主键
-     * @return 单条数据
-     */
-    @GetMapping("{id}")
-    public ResponseEntity<InspectBill> queryById(@PathVariable("id") Integer id) {
-        return ResponseEntity.ok(this.inspectBillService.queryById(id));
-    }
-
-    /**
-     * 新增数据
-     *
-     * @param inspectBill 实体
-     * @return 新增结果
-     */
-    @PostMapping
-    public ResponseEntity<InspectBill> add(InspectBill inspectBill) {
-        return ResponseEntity.ok(this.inspectBillService.insert(inspectBill));
-    }
-
-    /**
-     * 编辑数据
-     *
-     * @param inspectBill 实体
-     * @return 编辑结果
-     */
-    @PutMapping
-    public ResponseEntity<InspectBill> edit(InspectBill inspectBill) {
-        return ResponseEntity.ok(this.inspectBillService.update(inspectBill));
-    }
-
-    /**
-     * 删除数据
-     *
-     * @param id 主键
-     * @return 删除是否成功
-     */
-    @DeleteMapping
-    public ResponseEntity<Boolean> deleteById(Integer id) {
-        return ResponseEntity.ok(this.inspectBillService.deleteById(id));
-    }
 
 }
 
--- src/main/java/com/ikun/controller/RentBillController.java
+++ src/main/java/com/ikun/controller/RentBillController.java
@@ -18,67 +18,6 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("rentBill")
 public class RentBillController {
-    /**
-     * 服务对象
-     */
-    @Resource
-    private RentBillService rentBillService;
-
-    /**
-     * 分页查询
-     *
-     * @param rentBill 筛选条件
-     * @param pageRequest      分页对象
-     * @return 查询结果
-     */
-    @GetMapping
-    public ResponseEntity<Page<RentBill>> queryByPage(RentBill rentBill, PageRequest pageRequest) {
-        return ResponseEntity.ok(this.rentBillService.queryByPage(rentBill, pageRequest));
-    }
-
-    /**
-     * 通过主键查询单条数据
-     *
-     * @param id 主键
-     * @return 单条数据
-     */
-    @GetMapping("{id}")
-    public ResponseEntity<RentBill> queryById(@PathVariable("id") Integer id) {
-        return ResponseEntity.ok(this.rentBillService.queryById(id));
-    }
-
-    /**
-     * 新增数据
-     *
-     * @param rentBill 实体
-     * @return 新增结果
-     */
-    @PostMapping
-    public ResponseEntity<RentBill> add(RentBill rentBill) {
-        return ResponseEntity.ok(this.rentBillService.insert(rentBill));
-    }
-
-    /**
-     * 编辑数据
-     *
-     * @param rentBill 实体
-     * @return 编辑结果
-     */
-    @PutMapping
-    public ResponseEntity<RentBill> edit(RentBill rentBill) {
-        return ResponseEntity.ok(this.rentBillService.update(rentBill));
-    }
-
-    /**
-     * 删除数据
-     *
-     * @param id 主键
-     * @return 删除是否成功
-     */
-    @DeleteMapping
-    public ResponseEntity<Boolean> deleteById(Integer id) {
-        return ResponseEntity.ok(this.rentBillService.deleteById(id));
-    }
 
 }
 
