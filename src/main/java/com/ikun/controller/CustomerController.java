396f4a8eb14efde2ff2b152de6fad4f7d855f143
zmj <1639218509@qq.com>
2022-09-22 15:19:42 +0800

    客户controll(两个业务)

--- src/main/java/com/ikun/controller/CustomerController.java
+++ src/main/java/com/ikun/controller/CustomerController.java
@@ -2,11 +2,16 @@ package com.ikun.controller;
 
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ikun.dao.CustomerDao;
 import com.ikun.entity.Customer;
+import com.ikun.entity.vo.Constant;
 import com.ikun.service.CustomerService;
 import com.ikun.entity.Customer;
 import com.ikun.service.CustomerService;
-import org.springframework.data.domain.Page;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +27,27 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("customer")
 public class CustomerController {
+@Autowired
+    CustomerService customerService;
+@Autowired
+    CustomerDao customerDao;
+  @PostMapping("/save")
+    public Constant save(@RequestBody Customer customer){
+      Constant constant;
+      QueryWrapper<Customer> wrapper=new QueryWrapper();
+      wrapper.eq("card_id",customer.getCardId());
 
+     if(customerDao.selectOne(wrapper)!=null){constant=new Constant("ikun","客户已存在",null);}
+     else {customerDao.insert(customer);constant=new Constant("200","添加客户信息成功",customer);};
+      return  constant;
+  }
+  @GetMapping("/getall")
+    public Constant get(){
+      Constant constant;
+      QueryWrapper<Customer> queryWrapper=new QueryWrapper<>();
+      queryWrapper.gt("id",0);
+      constant=new Constant("200","返回成功",customerDao.selectList(queryWrapper));
+      return constant;
+  }
 }
 
