b420d5be6dde146f2a47a77e869aba8a1749b6b0
何浩天 <lovesun@haotian.com>
2022-09-22 17:38:41 +0800

    Car增删查改

--- src/main/java/com/ikun/controller/CarController.java
+++ src/main/java/com/ikun/controller/CarController.java
@@ -1,13 +1,16 @@
 package com.ikun.controller;
 
 import com.ikun.entity.Car;
+import com.ikun.entity.vo.Constant;
 import com.ikun.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * (Car)表控制层
@@ -18,6 +21,52 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("car")
 public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+    @GetMapping("/select")
+    public Constant selectCarList(){
+        Constant constant = new Constant();
+        List cars = carService.selectCarList();
+        constant.setMsg("查询成功！");
+        constant.setCode("200");
+        constant.setObj(cars);
+        return constant;
+    }
+
+    @PostMapping("/insert")
+    public Constant insertCar(@RequestBody Car car){
+        return carService.insertCar(car);
+    }
+
+    @GetMapping("/delete")
+    public Constant deleteCar(@RequestParam Integer id){
+        Constant constant = new Constant();
+        int result = carService.deleteCarById(id);
+        if (result==0){
+            constant.setCode("404");
+            constant.setMsg("删除失败。");
+            return constant;
+        }
+        constant.setCode("200");
+        constant.setMsg("删除成功。");
+        return constant;
+    }
+
+    @PostMapping("/update")
+    public Constant updateCar(@RequestBody Car car){
+        Constant constant = new Constant();
+        int result = carService.updateCar(car);
+        if (result==0){
+            constant.setCode("404");
+            constant.setMsg("更新失败。");
+            return constant;
+        }
+        constant.setCode("200");
+        constant.setMsg("更新成功。");
+        return constant;
+    }
 
 }
 
--- src/main/java/com/ikun/entity/Car.java
+++ src/main/java/com/ikun/entity/Car.java
@@ -44,64 +44,7 @@ public class Car implements Serializable {
     /**
      * 车辆出租情况 0未出租 1已出租
      */
-    private String condition;
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getEmpId() {
-        return empId;
-    }
-
-    public void setEmpId(String empId) {
-        this.empId = empId;
-    }
-
-    public String getLicenseNumber() {
-        return licenseNumber;
-    }
-
-    public void setLicenseNumber(String licenseNumber) {
-        this.licenseNumber = licenseNumber;
-    }
-
-    public String getRent() {
-        return rent;
-    }
-
-    public void setRent(String rent) {
-        this.rent = rent;
-    }
-
-    public String getMasterCard() {
-        return masterCard;
-    }
-
-    public void setMasterCard(String masterCard) {
-        this.masterCard = masterCard;
-    }
-
-    public String getCondition() {
-        return condition;
-    }
-
-    public void setCondition(String condition) {
-        this.condition = condition;
-    }
+    private String carCondition;
 
 }
 
